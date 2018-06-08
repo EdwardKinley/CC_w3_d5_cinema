ab937fdcf18108d250ee6fed04f513a82fa277db
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-06-08 15:22:47 +0100

    adds film.customers method

--- models/film.rb
+++ models/film.rb
@@ -53,6 +53,13 @@ class Film
     SqlRunner.run(sql, values)
   end
 
+  def customers()
+    sql = "SELECT customers.* FROM customers INNER JOIN tickets ON tickets.customer_id = customers.id WHERE film_id = $1"
+    values = [@id]
+    customers = SqlRunner.run(sql, values)
+    return Customer.map_items(customers)
+  end
+
   def self.delete_all()
     sql = "DELETE FROM films"
     SqlRunner.run(sql)
