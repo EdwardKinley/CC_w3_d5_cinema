227b60c110c61ed4464ae9c06c269d0520c87a82
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-06-08 13:48:06 +0100

    delete method (for individual entries) added

--- models/film.rb
+++ models/film.rb
@@ -19,6 +19,12 @@ class Film
     @id = film['id'].to_i
   end
 
+  def delete()
+    sql = "DELETE FROM films WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
   def self.delete_all()
     sql = "DELETE FROM films"
     SqlRunner.run(sql)
--- models/ticket.rb
+++ models/ticket.rb
@@ -20,6 +20,12 @@ class Ticket
     @id = ticket['id'].to_i
   end
 
+  def delete()
+    sql = "DELETE FROM tickets WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
   def self.delete_all()
     sql = "DELETE FROM tickets"
     SqlRunner.run(sql)
